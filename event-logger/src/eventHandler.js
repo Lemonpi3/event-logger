8a46290ad56abffa1d83cfe9baf449b2a93be1d8
Agustin Lehmann <96agustin.lehmann@gmail.com>
2024-07-20 11:12:21 -0300

    Added session time tracking, fixed bugs, and refactor

--- event-logger/src/eventHandler.js
+++ event-logger/src/eventHandler.js
@@ -1,8 +1,35 @@
 import axios from 'axios';
 
 const API_EVENT_URL = "http://127.0.0.1:8000/register-event"
+const API_SESSION_URL = "http://127.0.0.1:8000/session"
 
-function sendEvent(eventData){
+// modify this to filter out events on classes/ids
+const DIS_ALLOWED_IDS = [];
+const DIS_ALLOWED_CLASSES = ["App-header"];
+
+// session time variables
+let sessionStartTime = new Date();
+let sessionEndTime = null;
+
+
+const sendSessionData = async () => {
+    const sessionDuration = sessionEndTime ? (sessionEndTime - sessionStartTime) / 1000 : null;
+    const sessionData = {
+        startTime: sessionStartTime.toISOString(),
+        endTime: sessionEndTime ? sessionEndTime.toISOString() : null,
+        durationSeconds: sessionDuration,
+    };
+  
+    try {
+        await axios.post(API_SESSION_URL, sessionData);
+        console.log('Session data sent successfully:', sessionData);
+    } catch (error) {
+        console.error('Error sending session data:', error);
+    }
+};
+
+
+const sendEvent = (eventData) => {
     axios.post(API_EVENT_URL, eventData, {
         headers: {
             'Content-Type': 'application/json'
@@ -16,60 +43,85 @@ function sendEvent(eventData){
         });
 }
 
-export const handleEvent = (event) => {
-    const target = event.target;
 
-    if (target.id === "" || target.className === ""){
-        return;
-    }
 
-    // Check if the target is a clickable component or form
-    if (event.type === 'submit' && target.closest('form')) {
-        event.preventDefault();
+// <-- Handler Functions -->
+// They all receive the event and target and  return an event data json with 
+// type, tag, id, className, timestamp and any other niche from the type of object
 
-        const form = target.closest('form');
-        const formData = new FormData(form);
-        const formObject = {};
-        formData.forEach((value, key) => {
-        formObject[key] = value;
-        });
+// Also returns formData field as a json does not track fields named "password" 
+const handleFormEvent = (event, target) => {
+    event.preventDefault();
 
-        const eventData = {
+    const form = target.closest('form');
+    const formData = new FormData(form);
+    const formObject = {};
+
+    formData.forEach((value, key) => {
+        if (key !== "password") {
+            formObject[key] = value;
+        }
+    });
+
+    const eventData = {
         type: event.type,
         tag: form.tagName,
         id: form.id,
         className: form.className,
         timestamp: new Date().toISOString(),
         formData: formObject,
-        };
+    };
 
-        sendEvent(eventData);
-    }
+    return eventData
+}
 
-    else{
-        const eventData = {
+
+const handleButtonEvent = (event, target) => {
+    const eventData = {
         type: event.type,
         tag: target.tagName,
         id: target.id,
         className: target.className,
         timestamp: new Date().toISOString(),
-        };
+    };
 
-        if (event.type === 'submit') {
-            event.preventDefault();
+    return eventData;
+}
+// <-- end of Handler Functions -->
 
-            // Convert form data to a plain object
-            const formData = new FormData(target);
-            const formObject = {};
-            formData.forEach((value, key) => {
-                formObject[key] = value;
-            });
-            eventData.formData = formObject;
-        }
 
+// session time tracking
+
+// start timer
+window.addEventListener('load', () => {
+    sessionStartTime = new Date();
+});
+  
+// on exit page
+window.addEventListener('beforeunload', () => {
+    sessionEndTime = new Date();
+    sendSessionData();
+});
+
+
+export const handleEvent = (event) => {
+    const target = event.target;
+
+    const inValidEvent = (target.id === "" && target.className === "") ||
+                        DIS_ALLOWED_IDS.includes(target.id) ||
+                        DIS_ALLOWED_CLASSES.includes(target.className);
+
+    if (inValidEvent){
+        return;
+    }
+
+    if (event.type === 'submit' && target.closest('form')) {
+        const eventData = handleFormEvent(event, target);
         sendEvent(eventData);
     }
 
-
-        
+    if (event.type === "click") {
+        const eventData = handleButtonEvent(event, target);
+        sendEvent(eventData);
+    }        
 };
--- logger-api/main.py
+++ logger-api/main.py
@@ -18,6 +18,11 @@ class EventData(BaseModel):
     formData: dict = None
 
 
+class SessionData(BaseModel):
+    startTime: datetime
+    endTime: datetime = None
+    duration: float = None
+
 app = FastAPI()
 
 app.add_middleware(
@@ -34,10 +39,22 @@ async def capture_event(event_data: EventData):
     # anything that doesn't have id or class is skipped this should be on the app to prevent unnecessary calls
     # but here is just a failsafe to prevent useless data.
 
-    if event_data.id == "" or event_data.className == "":
+    if  is_empty_event(event_data):
         return
     
     # Log the event data
     logging.info(event_data.model_dump_json())
 
     return {"status": "success", "data": event_data}
+
+
+@app.post("/session")
+async def capture_session(session_data: SessionData):
+    logging.info(session_data.model_dump_json())
+    return {"status": "success", "data": session_data}
+
+
+def is_empty_event(event_data: EventData) -> bool:
+    return (
+        event_data.id == "" and event_data.className == ""
+    )
